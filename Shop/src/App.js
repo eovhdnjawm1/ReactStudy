d2cb8b859134a0704c4904d03309154a0deba8af
YoungJinGo <skyyj32@naver.com>
2022-05-01 04:33:22 +0900

    ✅ Shop Setting, Image Update

--- Shop/src/App.js
+++ Shop/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {Button, Navbar, Container, Nav, Row, Col} from 'react-bootstrap'
 import './App.css';
+import BackImg from './img/background.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Button variant="primary">Primary</Button>
+       <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">호두네점빵</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">홈</Nav.Link>
+      <Nav.Link href="#features">메뉴</Nav.Link>
+      <Nav.Link href="#pricing">주문하기</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <div className='main-bg' style={{backgroundImage : `url(${BackImg})`}}>
+
+  </div>
+    <div>
+    <Container>
+  <Row>
+    <Col sm>
+      <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="#" width="80%" />
+      <h4>상품명</h4>
+      <p>상품설명</p>
+      </Col>
+    <Col sm><img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="#" width="80%" />
+      <h4>상품명</h4>
+      <p>상품설명</p></Col>
+    <Col sm><img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="#" width="80%" />
+      <h4>상품명</h4>
+      <p>상품설명</p></Col>
+  </Row>
+</Container>
+    </div>
+
     </div>
   );
 }
