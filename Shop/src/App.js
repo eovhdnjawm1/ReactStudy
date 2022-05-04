07e751c62ee66fb7da598fe4418e504906da2676
YoungJinGo <skyyj32@naver.com>
2022-05-04 14:02:07 +0900

    ✨ Feat Detail Component Update

--- Shop/src/App.js
+++ Shop/src/App.js
@@ -1,16 +1,21 @@
 import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 import './App.css';
+import { useState } from "react"
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import DetailPage from './pages/detail.js';
 import BackImg from './img/background.jpg'
 import data from './data.js';
-import { useState } from "react"
-
 
 function App() {
-
-  let [shoes, setShoes] = useState(data);
-  console.log(shoes)
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
+  
   return (
     <div className="App">
+  
+
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
       
       <Button variant="primary">Primary</Button>
       <Navbar bg="dark" variant="dark">
@@ -23,39 +28,42 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg' style={{ backgroundImage: `url(${BackImg})` }}>
+          <div className='main-bg' style={{ backgroundImage: `url(${BackImg})` }}></div>
+      <Routes>
+        <Route path="/" element={<div>메인페이지</div>} />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
 
-      </div>
       <div>
-        <Container>
-          <Row>
-
-            {
-              shoes.map(function(val, i) {
-                return (
-                  <Col sm className="shoes" key={i}>
-                    <MenusCom shoes={shoes}  i={i} />
-                  </Col>
-                )
-              })
-            }
-          </Row>
-        </Container>
-      </div>
-
+		  <Container>
+			<Row>
+  
+			  {
+				shoes.map(function(val, i) {
+				  return (
+					<Col sm className="shoes" key={i}>
+					  <MenusCom shoes={shoes}  i={i} />
+					</Col>
+				  )
+				})
+			  }
+			</Row>
+		  </Container>
+		</div>
     </div>
   );
 }
 
-
 function MenusCom(props) {
-  return (
-      <div>
-        <img src={`${props.shoes[props.i].image}`} alt="#" width="80%" />
-        <h4>{props.shoes[props.i].title}</h4>
-        <p>{props.shoes[props.i].price}</p>
-      </div>
-  )
-}
+	return (
+		<div>
+		  <img src={`${props.shoes[props.i].image}`} alt="#" width="80%" />
+		  <h4>{props.shoes[props.i].title}</h4>
+		  <p>{props.shoes[props.i].price}</p>
+		</div>
+	)
+  }
+
+
 
 export default App;
