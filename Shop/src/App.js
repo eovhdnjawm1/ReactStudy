d5fd888e74f4f54d41c450bf16bb705656bf4d36
YoungJinGo <skyyj32@naver.com>
2022-05-04 12:30:05 +0900

    ✨ menus Component Setting

--- Shop/src/App.js
+++ Shop/src/App.js
@@ -1,44 +1,61 @@
-import {Button, Navbar, Container, Nav, Row, Col} from 'react-bootstrap'
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 import './App.css';
 import BackImg from './img/background.jpg'
+import data from './data.js';
+import { useState } from "react"
+
 
 function App() {
+
+  let [shoes, setShoes] = useState(data);
+  console.log(shoes)
   return (
     <div className="App">
-       <Button variant="primary">Primary</Button>
-       <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">호두네점빵</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">홈</Nav.Link>
-      <Nav.Link href="#features">메뉴</Nav.Link>
-      <Nav.Link href="#pricing">주문하기</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-  <div className='main-bg' style={{backgroundImage : `url(${BackImg})`}}>
+      
+      <Button variant="primary">Primary</Button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">호두네점빵</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">홈</Nav.Link>
+            <Nav.Link href="#features">메뉴</Nav.Link>
+            <Nav.Link href="#pricing">주문하기</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg' style={{ backgroundImage: `url(${BackImg})` }}>
 
-  </div>
-    <div>
-    <Container>
-  <Row>
-    <Col sm>
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="#" width="80%" />
-      <h4>상품명</h4>
-      <p>상품설명</p>
-      </Col>
-    <Col sm><img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="#" width="80%" />
-      <h4>상품명</h4>
-      <p>상품설명</p></Col>
-    <Col sm><img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="#" width="80%" />
-      <h4>상품명</h4>
-      <p>상품설명</p></Col>
-  </Row>
-</Container>
-    </div>
+      </div>
+      <div>
+        <Container>
+          <Row>
+
+            {
+              shoes.map(function(val, i) {
+                return (
+                  <Col sm className="shoes" key={i}>
+                    <MenusCom shoes={shoes}  i={i} />
+                  </Col>
+                )
+              })
+            }
+          </Row>
+        </Container>
+      </div>
 
     </div>
   );
+}
+
+
+function MenusCom(props) {
+  return (
+      <div>
+        <img src={`${props.shoes[props.i].image}`} alt="#" width="80%" />
+        <h4>{props.shoes[props.i].title}</h4>
+        <p>{props.shoes[props.i].price}</p>
+      </div>
+  )
 }
 
 export default App;
