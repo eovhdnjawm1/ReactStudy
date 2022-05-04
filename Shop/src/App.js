798575fa74b6c7f671fc40180d99d70be06a3b70
YoungJinGo <skyyj32@naver.com>
2022-05-04 14:48:00 +0900

    ✨ Feat About, Event Component Update

--- Shop/src/App.js
+++ Shop/src/App.js
@@ -9,60 +9,83 @@ import data from './data.js';
 function App() {
   let [shoes] = useState(data);
   let navigate = useNavigate();
-  
+
   return (
     <div className="App">
-  
-
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
       
-      <Button variant="primary">Primary</Button>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">호두네점빵</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">홈</Nav.Link>
-            <Nav.Link href="#features">메뉴</Nav.Link>
-            <Nav.Link href="#pricing">주문하기</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-          <div className='main-bg' style={{ backgroundImage: `url(${BackImg})` }}></div>
-      <Routes>
-        <Route path="/" element={<div>메인페이지</div>} />
-        <Route path="/detail" element={<DetailPage />} />
-      </Routes>
 
-      <div>
-		  <Container>
-			<Row>
-  
-			  {
-				shoes.map(function(val, i) {
-				  return (
-					<Col sm className="shoes" key={i}>
-					  <MenusCom shoes={shoes}  i={i} />
-					</Col>
-				  )
-				})
-			  }
-			</Row>
-		  </Container>
-		</div>
+        <Link to="/">홈</Link>
+        <Link to="/detail">상세페이지</Link>
+
+        <Button variant="primary">Primary</Button>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">호두네점빵</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link onClick={() => { navigate('/') }}>홈</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/detail') }}>메뉴</Nav.Link>
+              <Nav.Link onClick={() => { navigate(-1) }}>주문하기</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        
+        <Routes>
+        <Route  path="/" element={ <Container style={{  padding:"0"}}>
+          <div className='main-bg' style={{ width:"100vw", objectFit:"cover",  backgroundImage: `url(${BackImg})` }}></div>
+          <Row>
+            {
+              shoes.map(function (val, i) {
+                return (
+                  <Col sm className="shoes" key={i}>
+                    <MenusCom shoes={shoes} i={i} />
+                  </Col>
+                )
+              })
+            }
+          </Row>
+        </Container>} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        
+        <Route path="*" element={<h1>없는 페이지 입니다.</h1>} />
+      </Routes>
     </div>
   );
 }
 
+function Event() {
+  return (
+    <div>
+      <h2>오늘의 이벤트</h2>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
 function MenusCom(props) {
-	return (
-		<div>
-		  <img src={`${props.shoes[props.i].image}`} alt="#" width="80%" />
-		  <h4>{props.shoes[props.i].title}</h4>
-		  <p>{props.shoes[props.i].price}</p>
-		</div>
-	)
-  }
+  return (
+    <div>
+      <img src={`${props.shoes[props.i].image}`} alt="#" width="80%" />
+      <h4>{props.shoes[props.i].title}</h4>
+      <p>{props.shoes[props.i].price}</p>
+    </div>
+  )
+}
 
 
 
