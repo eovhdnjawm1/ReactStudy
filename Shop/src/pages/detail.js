808ad79be5b5f11cfeaa301941b5ced42f3caf1d
YoungJinGo <skyyj32@naver.com>
2022-05-04 17:25:40 +0900

    ✨ Feat Styled Component Setting

--- Shop/src/pages/detail.js
+++ Shop/src/pages/detail.js
@@ -1,19 +1,38 @@
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 // data.js 에 있는 id 값을 불러와야함
 // 지금은 URL 0이면 그냥 id 값이 뭐든 가져오는데
 // id 값에 맞는걸 가져와야한다
+
+let YellowBtn = styled.button`
+	background : ${props => props.bg};
+	color : ${ props => props.bg == 'blue' ? 'pink' : 'black' };
+	padding : 10px;
+
+`
+
+let NewBtn = styled.button(YellowBtn)`
+
+`
+
+let BlackBox = styled.div`
+	background : black;
+	padding: 20px;
+	width: 25px;
+	height: 25px;
+`
 
 function DetailApp(props) {
 	let {id} = useParams();
 	let 찾은상품 = props.shoes.find(function(x){
 		return x.id == id;
 	});	
-	
-	
-
 	return (
 		<div className="container">
+			<YellowBtn>버튼</YellowBtn>
+			<YellowBtn bg="blue">버튼</YellowBtn>
+			<BlackBox></BlackBox>
 			<div className="row">
 				<div className="col-md-6">
 					<img src={`${찾은상품.image}`} width="100%" alt="#"/>
