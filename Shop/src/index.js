ad7eb74ac5cc911eb0f6a5c3bbf9ae8331ed192c
YoungJinGo <skyyj32@naver.com>
2022-05-06 15:22:25 +0900

    ✨ Feat alert state change Setting

--- Shop/src/index.js
+++ Shop/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import { combineReducers, createStore } from 'redux';
 
 
@@ -14,53 +14,65 @@ let stanData = [
   {
     id: 0,
     name: '멋진신발',
-    quan : 2000,
+    quan: 2000,
   },
   {
     id: 1,
     name: '예쁜신발',
-    quan : 2500,
+    quan: 2500,
   }
-] 
+]
 
 let alert초기값 = true;
 
-function reducer2(state = alert초기값, action){
-  
-  return state;
+function reducer2(state = alert초기값, action) {
+  if (action.type === '닫힘') {
+    let copy = alert초기값;
+    copy = false;
+    return copy
+  }
+  else if (action.type === '열림') {
+    let copy = alert초기값;
+    copy = true;
+    return copy
+  }
+  else{
+    return state;
+  }
 }
 
-function reducer(state = stanData, action){
-  if( action.countChange === '수량증가') {
+
+function reducer(state = stanData, action) {
+  if (action.countChange === '수량증가') {
     // 데이터 수정 조건
     let copy = [...stanData];
     copy[0].id++;
 
     return copy
-  } 
-  else if ( action.countChange === '수량감소'){
+  }
+  else if (action.countChange === '수량감소') {
     let copy = [...stanData];
     copy[0].id--;
 
     return copy
   }
-  else{
+  else {
     return state
   }
 }
 
-let store = createStore(combineReducers({reducer, reducer2}))
+let store = createStore(combineReducers({ reducer, reducer2 }))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-     <App />
-     </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
-  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
