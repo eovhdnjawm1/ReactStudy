5db319f71fd03bc819cae6c213a25a2e271dd92e
YoungJinGo <skyyj32@naver.com>
2022-05-06 21:29:52 +0900

    ✅ Feat Server Setting

--- server/server.js
+++ server/server.js
@@ -6,3 +6,11 @@ const http = require('http').createServer(app);
 http.listen(8080, function () {
   console.log('listening on 8080')
 }); 
+
+// 미들웨어
+// static 파일들이 어디있습니다 명시 ( html css js 등)
+app.use(express.static( path.join(__dirname, 'public')))
+
+app.get('/', function(요청, 응답) {
+	응답.sendFile( path.join(__dirname, 'public/index.html'))
+})
